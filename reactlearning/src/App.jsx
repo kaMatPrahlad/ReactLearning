66de2e109c101583238eb33acd02ee68a6ae3d65
kaMatPrahlad <kamatprahlad43@gmail.com>
2026-02-03 17:14:25 +0530

    Initialize React project with Vite, add UserCard component,

---
+++ reactlearning/src/App.jsx
@@ -0,0 +1,15 @@
+import UserCard from "./components/UserCard";
+
+function App() {
+  return (
+    <>
+      {/* //What is Components / Props in React */}
+      {/* A component is a reusable piece of UI—think “a small, focused function that returns JSX.” */}
+      {/* Props are the inputs you pass into a component, like arguments to a function, so you can configure how it renders. */}
+
+      <UserCard />
+    </>
+  );
+}
+
+export default App;
