b886d09c649f7abc4ea3dd40f2f3bdbd1e6bf778
kaMatPrahlad <kamatprahlad43@gmail.com>
2026-02-13 19:51:33 +0530

    Refactor UseRef component: implement stopwatch functionality with start, stop, and reset actions; remove unused code

--- reactlearning/src/components/UseRef.jsx
+++ reactlearning/src/components/UseRef.jsx
@@ -1,23 +1,48 @@
+import { current } from "@reduxjs/toolkit";
 import React, { useEffect, useRef, useState } from "react";
 
 const UseRef = () => {
-  const [count, setCount] = useState(0);
-  let val = useRef(0);
+  //   const [count, setCount] = useState(0);
+  const [time, setTime] = useState(0);
+  const timeRef = useRef(null);
 
-  function handleClick() {
-    val.current = val.current + 1;
-    console.log("value of vale", val.current);
-    setCount(count + 1);
+  //   let btnRef = useRef();
+  function startTime() {
+    timeRef.current = setInterval(() => {
+      setTime((time) => time + 1);
+    }, 1000);
+  }
+  function stopTime() {
+    clearInterval(timeRef.current);
+    timeRef.current = null;
+  }
+  function resetTime() {
+    stopTime();
+    setTime(0);
   }
 
-  useEffect(() => {
-    console.log("I will run on every render.");
-  });
+  //   function changeColor() {
+  //     btnRef.current.style.backgroundColor = "red";
+  //   }
+  //   function handleClick() {
+  //     val.current = val.current + 1;
+  //     console.log("value of vale", val.current);
+  //     setCount(count + 1);
+  //   }
+
+  //   useEffect(() => {
+  //     console.log("I will run on every render.");
+  //   });
 
   return (
     <div>
       <h1>UseRef React Hook</h1>
       {/* <p>
+        useRef is a React Hook that lets you:
+
+        Access DOM elements directly
+
+        Store values that don’t cause re-render when changed
         useRef is a React Hook that allows you to persist values across renders
         without causing a re-render when the value changes. It provides a
         mutable ref object that can hold any value, and its .current property is
@@ -25,8 +50,27 @@ const UseRef = () => {
       </p> */}
 
       <div>
-        <button onClick={handleClick}>Click me</button>
-        <span>{count}</span>
+        {/* <button onClick={handleClick} ref={btnRef}>
+          Increment
+        </button>
+        <br />
+        <span onChange={changeColor}>Count: {count}</span>
+        <br />
+        <button onClick={changeColor}>Decrement</button> */}
+
+        <div>
+          <h1>Stop Watch Using UseRef</h1>
+          <button onClick={startTime}>start</button>
+          <br />
+          <br />
+          <button onClick={stopTime}>Stop</button>
+          <br />
+          <br />
+          <button onClick={resetTime}>Reset</button>
+          <br />
+          <br />
+          <span>StopWatch: {time}</span>
+        </div>
       </div>
     </div>
   );
